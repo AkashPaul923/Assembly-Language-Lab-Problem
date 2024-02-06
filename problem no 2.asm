45d773002ab6720c5fb8af72d840c1a05e71b46d
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-02-06 23:39:42 +0600

    problem 2

---
+++ problem no 2.asm
@@ -0,0 +1,40 @@
+.MODEL SHALL
+
+.STACK 100H
+
+.DATA
+
+.CODE
+
+main proc
+
+mov ah,1
+int 21h 
+mov bl,al
+
+mov ah, Eh 
+mov al,0Dh 
+int 10h 
+mov al,0Ah 
+int 10h
+
+cmp bl,"y" 
+JE display
+
+cmp bl,"Y" 
+JE display
+
+JMP exit
+
+display:
+
+mov ah,2 
+mov dl,bl 
+int 21h
+
+exit: 
+mov ah, 4ch 
+int 21h
+
+main endp 
+END MAIN
